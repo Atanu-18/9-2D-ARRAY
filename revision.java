9e726195179d38cca50652bb99575fde8c33c4cb
atanu mandal <atanu2697@gmail.com>
2025-06-18 13:45:02 +0530

    updated code

--- revision.java
+++ revision.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 
 public class revision {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int rows = sc.nextInt();
         int cols = sc.nextInt();
-        int [][] numbers = new int[rows][cols];
 
-        //ip
+        int[][] numbers = new int[rows][cols];
+
+        //i-p
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                numbers[i][j] = sc.nextInt();
-            }
+           for (int j = 0; j < cols; j++) {
+            numbers[i][j] = sc.nextInt();
+           } 
         }
 
+        System.out.println("the x is : ");
+        int x = sc.nextInt();
 
-        //op
+        //o-p
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                System.out.print(numbers[i][j] + " ");
+                if (numbers[i][j] == x) {
+                    System.out.println("x is found at : (" + i + "," + j + ")");
+                }
             }
-            System.out.println();
         }
 
         sc.close();
