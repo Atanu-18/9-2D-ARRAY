487f838e814d8f12cf89a381ffebc512b2f4d76a
atanu mandal <atanu2697@gmail.com>
2025-06-18 13:36:47 +0530

    add

--- revision.java
+++ revision.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class revision {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int [][] numbers = new int[rows][cols];
+
+        //ip
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+
+        //op
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
